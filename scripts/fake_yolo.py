a901a0681e45887986690dd35a014e6cece93a07
jdw169 <roger16986@gmail.com>
2026-03-18 18:11:17 -0700

    new file:   scripts/fake_yolo.py 	modified:   scripts/pick_and_place.py

---
+++ scripts/fake_yolo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import rospy
+from darknet_ros_msgs.msg import BoundingBoxes, BoundingBox
+
+def start_fake_camera():
+    rospy.init_node('fake_yolo_node')
+    # Publish to the exact topic your main script is listening to
+    pub = rospy.Publisher('/darknet_ros/bounding_boxes', BoundingBoxes, queue_size=10)
+    rate = rospy.Rate(10) # Publish at 10 Hz
+
+    rospy.loginfo("Fake YOLO is running! Broadcasting dummy cube...")
+
+    while not rospy.is_shutdown():
+        msg = BoundingBoxes()
+        
+        # Create a fake cube based on your real data
+        fake_cube = BoundingBox()
+        fake_cube.Class = "kite"
+        fake_cube.xmin = 335
+        fake_cube.xmax = 483
+        fake_cube.ymin = 36
+        fake_cube.ymax = 269
+        fake_cube.z = 218.0  # 171 mm depth
+        
+        # Add it to the message array and publish
+        msg.bounding_boxes.append(fake_cube)
+        pub.publish(msg)
+        
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        start_fake_camera()
+    except rospy.ROSInterruptException:
+        pass
--- scripts/pick_and_place.py
+++ scripts/pick_and_place.py
@@ -7,27 +7,34 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 import copy
 import tf.transformations
+import tf2_ros
+import tf2_geometry_msgs
+from geometry_msgs.msg import PointStamped
+from image_geometry import PinholeCameraModel
+from sensor_msgs.msg import CameraInfo
+from darknet_ros_msgs.msg import BoundingBoxes
+from kortex_driver.srv import *
+from kortex_driver.msg import *
 
-#standby_joint_pose = [0.663,-0.440,1.413,-1.571,-1.290,0.663]
-standby_pose = [0.320,0.165,0.323,3.14,0,3.14]
+standby_pose = [0.320, 0.165, 0.323, 3.14, 0, 3.14]
 pick_up_x = 0
 pick_up_y = 0
 pick_yaw = 0
 drop_off_x = 0
 drop_off_y = 0
 
-class pick_cube:
+class pick_cube_vision:
 
     def __init__(self):
-        # 1. Initialize C++ backend and rospy
+        # Initialize C++ backend and rospy
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node('pick_place_by_color', anonymous=True)
 
-        # 2. Define the namespace AND the exact path to the robot description
+        # Define the namespace AND the exact path to the robot description
         robot_ns = "my_gen3_lite"
         robot_desc = "my_gen3_lite/robot_description"
 
-        # 3. Instantiate MoveIt objects, passing BOTH the namespace and the desc
+        # Instantiate MoveIt objects, passing BOTH the namespace and the desc
         self.robot = moveit_commander.RobotCommander(robot_description=robot_desc, ns=robot_ns)
         self.scene = moveit_commander.PlanningSceneInterface(ns=robot_ns)
         
@@ -38,15 +45,25 @@ class pick_cube:
         self.move_group.set_max_acceleration_scaling_factor(0.8)
         # Give the scene a second to initialize before acting
         rospy.sleep(2) 
-        
-        # 4. Move to home position
+        """
+        # clear faults
+        self.robot_name = rospy.get_param('~robot_name', "my_gen3lite") 
+        clear_faults_full_name = '/' + self.robot_name + '/base/clear_faults'
+        rospy.wait_for_service(clear_faults_full_name)
+        self.clear_faults = rospy.ServiceProxy(clear_faults_full_name, Base_ClearFaults)
+
+        rospy.loginfo("Clear faults")
+        self.clear_faults()
+        rospy.sleep(2.5)
+        """
+        # Move robot to home position
         rospy.loginfo("Moving arm to home position...")
         self.move_group.set_named_target("home")
         self.move_group.go(wait=True)
-        
-        # 5. Move to standby pose
-        #self.move_group.set_joint_value_target(standby_joint_pose)
-
+        # set move tolerance
+        self.move_group.set_goal_position_tolerance(0.01)
+        self.move_group.set_goal_orientation_tolerance(0.01)
+        # Move to standby pose
         rospy.loginfo("Moving arm to standby position...Tilt end effector completely upright....")
         self.move_group.set_pose_target(standby_pose)
         success = self.move_group.go(wait=True)
@@ -64,15 +81,26 @@ class pick_cube:
         rospy.loginfo(f"Current RPY (in radians): Roll={roll:.3f}, Pitch={pitch:.3f}, Yaw={yaw:.3f}")    
         self.move_group.stop()
 
-        # 6. Start the pick and place sequence
-        self.move_group.set_goal_position_tolerance(0.01)
-        self.move_group.set_goal_orientation_tolerance(0.01)
-        pick_up_x = 0.35
-        pick_up_y = 0.1
-        pick_yaw = 0
-        drop_off_x = -0.2
-        drop_off_y = 0.15
-        self.pick_and_place(pick_up_x,pick_up_y,pick_yaw,drop_off_x,drop_off_y)
+        # Set up the TF2 Listener to translate coordinates
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        
+        # Get the camera's lens properties (intrinsics)
+        #self.camera_model = PinholeCameraModel()
+        #cam_info = rospy.wait_for_message('/camera/color/camera_info', CameraInfo)
+        #self.camera_model.fromCameraInfo(cam_info)
+
+        self.camera_model = PinholeCameraModel()
+        dummy_info = CameraInfo()
+        dummy_info.K = [615.0, 0.0, 320.0, 0.0, 615.0, 240.0, 0.0, 0.0, 1.0]
+        dummy_info.P = [615.0, 0.0, 320.0, 0.0, 0.0, 615.0, 240.0, 0.0, 0.0, 0.0, 1.0, 0.0]
+        self.camera_model.fromCameraInfo(dummy_info)
+        # flag to indicating the robot is busy with motion
+        self.ispicking = False
+
+        # Subscribe to Darknet bounding boxes
+        self.bbox_sub = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.vision_callback)
+
 
     def operate_gripper(self, state):
         # 1. Get the active motorized joint
@@ -84,8 +112,8 @@ class pick_cube:
         if state == "opened":
             target_dict[motorized_joint] = 0.80   
         elif state == "closed":
-            target_dict[motorized_joint] = 0.08  
-            
+            target_dict[motorized_joint] = 0.20 
+
         rospy.loginfo(f"Commanding {motorized_joint} to: {target_dict[motorized_joint]}")
         
         # 3. Execute using the dictionary target
@@ -102,26 +130,8 @@ class pick_cube:
             
         rospy.sleep(1) # Give the physics engine a second to settle
 
-    def turn_end_effector(self,yaw):
-        current_pose = self.move_group.get_current_pose().pose
-        quaternion = [
-            current_pose.orientation.x,
-            current_pose.orientation.y,
-            current_pose.orientation.z,
-            current_pose.orientation.w
-        ]
-        (roll, pitch, current_yaw) = tf.transformations.euler_from_quaternion(quaternion)
-        target_pose_list = [current_pose.position.x,
-                            current_pose.position.y,
-                            current_pose.position.z,
-                            roll, pitch, yaw]
-        self.move_group.set_pose_target(target_pose_list)
-        success = self.move_group.go(wait=True)
-        if not success:
-            rospy.logerr("Failed to turn end effctor! Stopping sequence.")
-            return
 
-    def pick_and_place(self,pick_up_x,pick_up_y,pick_yaw,drop_off_x,drop_off_y):
+    def pick_and_place(self,pick_up_x,pick_up_y,drop_off_x,drop_off_y):
         rospy.loginfo("Starting Pick-and-Place sequence...")
         #standby_pose = self.move_group.get_current_pose().pose
 
@@ -132,6 +142,7 @@ class pick_cube:
         target_pose = self.move_group.get_current_pose().pose
         target_pose.position.x = pick_up_x
         target_pose.position.y = pick_up_y
+        target_pose.position.z = 0.015
         rospy.loginfo("Move clamp closer...")
         self.move_group.set_pose_target(target_pose)
         success = self.move_group.go(wait=True)
@@ -139,15 +150,11 @@ class pick_cube:
             rospy.logerr("Failed to move clamp closer! Stopping sequence.")
             return # Exit the function so it doesn't keep going
 
-        # 3. turn end effector to align with the part
-
-        self.turn_end_effector(pick_yaw)
 
         # 4. Dip down to "grab" the part
-        
         waypoints = []
         wpose = self.move_group.get_current_pose().pose
-        wpose.position.z = 0.045
+        #wpose.position.z -= 0.2
         waypoints.append(copy.deepcopy(wpose))
         (plan,fraction) = self.move_group.compute_cartesian_path(waypoints, 0.01)
         if fraction < 0.9:
@@ -176,8 +183,9 @@ class pick_cube:
         self.move_group.go(wait=True)
 
         # 7. Move to the intermediat waypoint (x=0 y=0.3)
-        target_pose.position.y = 0.3
-        target_pose.position.x = 0.0
+        target_pose.position.x = 0.3
+        target_pose.position.y = 0.0
+        
         rospy.loginfo("Move to intermediat waypoint")
         self.move_group.set_pose_target(target_pose)
         self.move_group.go(wait=True)
@@ -193,15 +201,62 @@ class pick_cube:
         self.operate_gripper("opened")
 
         # 10. Return to the Standby Position
+        
         rospy.loginfo("Returning to standby position...")
-        self.move_group.set_pose_target(standby_pose)
+        self.move_group.set_named_target("home")
+        #self.move_group.set_pose_target(standby_pose)
         self.move_group.go(wait=True)
         self.operate_gripper("closed")
         rospy.loginfo("Pick-and-Place complete! ...")
 
+    def vision_callback(self, msg):
+        # if the robot is in motion, return
+        if self.ispicking == True:
+            return
+        
+        for box in msg.bounding_boxes:
+            if box.Class in ["kite","tvmonitor","cup"]:
+                # 1. Find the center pixel of the bounding box
+                self.ispicking = True
+                center_u = (box.xmin + box.xmax) / 2.0
+                center_v = (box.ymin + box.ymax) / 2.0
+                depth_z = box.z / 1000 # Your depth value in meters
+                
+                # 2. Convert the 2D pixel into a 3D Ray, then multiply by Depth
+                # This gives (X, Y, Z) in the camera's physical frame
+                ray = self.camera_model.projectPixelTo3dRay((center_u, center_v))
+                camera_x = ray[0] * depth_z
+                camera_y = ray[1] * depth_z
+                camera_z = depth_z
+                
+                # 3. Package this coordinate into a ROS PointStamped message
+                target_point_camera = PointStamped()
+                target_point_camera.header.frame_id = "camera_color_optical_frame"
+                target_point_camera.header.stamp = rospy.Time.now()
+                target_point_camera.point.x = camera_x
+                target_point_camera.point.y = camera_y
+                target_point_camera.point.z = camera_z
+                
+                # 4. Use TF2 to translate from the camera frame to the robot base frame
+                try:
+                    # Wait up to 1 second for the transform tree to be ready
+                    target_point_base = self.tf_buffer.transform(target_point_camera, "my_gen3_lite/base_link", rospy.Duration(1.0))
+                    
+                    rospy.loginfo(f"Cube found at Robot X:{target_point_base.point.x:.2f}, Y:{target_point_base.point.y:.2f}")
+                    
+                    # 5. SEND TO MOVEIT!
+                    # Now you can pass these exact X and Y coordinates to your pick_and_place function
+                    # self.pick_and_place(target_point_base.point.x, target_point_base.point.y,0.15,-0.25)
+                    
+                except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+                    rospy.logerr(f"TF Transform failed: {e}")
+                self.ispicking  = False
+                break
+
+
 if __name__ == '__main__':
     try:
-        pick_cube()
-        #rospy.spin()
+        pick_cube_vision()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
